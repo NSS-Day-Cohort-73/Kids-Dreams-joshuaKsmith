cc3bdb13f3ef10d45a8abad95fa34011da34eff3
unknown <mtgmirage@protonmail.com>
2024-09-26 14:56:45 -0500

    built event listener in Kids.js to display a child's wish when their name is clicked

--- scripts/Kids.js
+++ scripts/Kids.js
@@ -2,11 +2,23 @@ import { getChildren } from "./database.js"
 
 const children = getChildren()
 
+document.addEventListener(
+    "click",
+    (theClickEvent) => {
+        const theClickedOnChild = theClickEvent.target
+        if (theClickedOnChild.dataset.type === "child") {
+            const childName = theClickedOnChild.dataset.name
+            const childWish = theClickedOnChild.dataset.wish
+            window.alert(`${childName}'s wish is ${childWish}`)
+        }
+    }
+)
+
 export const Kids = () => {
     let html = "<ol>"
 
     for (const child of children) {
-        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child.name}</li>`
+        html += `<li data-id="${child.id}" data-name="${child.name}" data-type="child" data-wish="${child.wish}">${child.name}</li>`
     }
 
     html += "</ol>"
